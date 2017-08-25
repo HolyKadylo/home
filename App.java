d53fb6bf3fdf52a0a8146a2620d6dbac879236ff
HolyKadylo <pivas47@gmail.com>
2017-08-25 22:51:26 +0300

    adding infinite loop

--- App.java
+++ App.java
@@ -1,9 +1,20 @@
+import java.lang.Thread;
+
 class App{
 	public static void main(String[] args){
-		try{
-			System.out.println("-->Hello, arg:" + args[0]);
-		} catch (Exception e){
-			System.out.println("-->No args");
+		while(true){
+			try{
+				System.out.println("-->Sleeping 10 s");
+				Thread.currentThread().sleep(10000);
+				try{
+					System.out.println("-->Hello, arg:" + args[0]);
+				} catch (Exception e){
+					System.out.println("-->No args");
+				}
+			} catch (InterruptedException ie){
+				System.out.println("-->Interrupted");
+				break;
+			}
 		}
 	}
 }
